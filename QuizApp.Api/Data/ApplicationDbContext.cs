35ba3ebc09e28dcf645ab36df8e1a8c2ea41689b
James Absalem Lintu <jamesabsalem@gmail.com>
2023-11-17 20:26:00 +0600

    working with db connection

---
+++ QuizApp.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QuizApp.Api.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+    }
+}
