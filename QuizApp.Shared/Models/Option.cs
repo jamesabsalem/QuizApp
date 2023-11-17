36d90a5d14ddd2d58e8f749cb839c0ecf0d77b9b
Tania Akther <taniaakther2113@gmail.com>
2023-11-17 20:22:43 +0600

    Add model

---
+++ QuizApp.Shared/Models/Option.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp.Shared.Models
+{
+    public class Option
+    {
+        public int OptionId { get; set; }
+        public int QuestionId { get; set; }
+        public string OptionText { get; set; }
+        public bool IsAnswer { get; set; }
+    }
+}
---
+++ QuizApp.Shared/Models/Question.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp.Shared.Models
+{
+    public class Question
+    {
+        public int QuestionId { get; set; }
+        public int QuizId { get; set; }
+        public int QuestionTypeId { get; set; }
+        public string QuestionText { get; set; }
+        public string Url { get; set; }
+        public int TimeLemit { get; set; }
+        public int Point { get; set; }
+    }
+}
