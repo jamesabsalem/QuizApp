b238f8cc9e2b662320c8af85dc0b148ccab78113
James Absalem Lintu <jamesabsalem@gmail.com>
2023-11-17 15:17:23 +0600

    worked with boostrap toastr

--- QuizApp.Client/Pages/Home.razor.cs
+++ QuizApp.Client/Pages/Home.razor.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using ERP.Web.Helper;
+using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 
 namespace QuizApp.Client.Pages
 {
     public partial class Home
     {
+        [Inject] IJSRuntime _jsRuntime { get; set; }
+        private async Task onClick()
+        {
+            await _jsRuntime.ToastrSuccess("test");
+        }
     }
 }
