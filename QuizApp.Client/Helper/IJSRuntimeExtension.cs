3a63705c97f59b130b355db8db9ef11039bf50b4
Tania Akther <taniaakther2113@gmail.com>
2023-11-17 12:43:23 +0600

    Add folder

---
+++ QuizApp.Client/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.JSInterop;
+
+namespace QuizApp.Client.Helper
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async ValueTask ToastSuccess(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+
+        }
+        public static async ValueTask ToastrError(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+        public static async ValueTask ToastrWarning(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "warning", message);
+        }
+        public static async ValueTask ToastrInfo(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "info", message);
+        }
+    }
+}
