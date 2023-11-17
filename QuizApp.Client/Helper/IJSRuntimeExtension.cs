ef1a9fa56415ab977f4d62ca335880746ad7a99c
Tania Akther <taniaakther2113@gmail.com>
2023-11-17 13:10:03 +0600

    working with toster

--- QuizApp.Client/Helper/IJSRuntimeExtension.cs
+++ QuizApp.Client/Helper/IJSRuntimeExtension.cs
@@ -4,10 +4,9 @@ namespace QuizApp.Client.Helper
 {
     public static class IJSRuntimeExtension
     {
-        public static async ValueTask ToastSuccess(this IJSRuntime jsRuntime, string message)
+        public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message)
         {
             await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
-
         }
         public static async ValueTask ToastrError(this IJSRuntime jsRuntime, string message)
         {
---
+++ QuizApp.Client/Pages/Home.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using QuizApp.Client.Helper;
+
+namespace QuizApp.Client.Pages
+{
+    public partial class Home
+    {
+        [Inject] IJSRuntime _JsRuntime { get; set; }
+        private async Task test()
+        {
+            await _JsRuntime.ToastrSuccess("Success");
+        }
+    }
+}
