584dd1f4c5e1e9ddde80cec889790a74e9571025
Tania Akther <taniaakther2113@gmail.com>
2023-11-17 13:59:49 +0600

    remove toast

--- QuizApp.Client/Pages/Home.razor.cs
+++ QuizApp.Client/Pages/Home.razor.cs
@@ -1,15 +1,9 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
-using QuizApp.Client.Helper;
 
 namespace QuizApp.Client.Pages
 {
     public partial class Home
     {
-        [Inject] IJSRuntime _JsRuntime { get; set; }
-        private async Task test()
-        {
-            await _JsRuntime.ToastrSuccess("Success");
-        }
     }
 }
