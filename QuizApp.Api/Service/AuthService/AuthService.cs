3b861b7617a97811b059e5112ed690a5a12f7b14
Tania Akther <taniaakther2113@gmail.com>
2023-11-18 20:24:13 +0600

    Add Auth Service

---
+++ QuizApp.Api/Service/AuthService/AuthService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizApp.Api.Data;
+using QuizApp.Api.Handler;
+using QuizApp.Shared.Helper;
+using QuizApp.Shared.Models.Dto;
+
+namespace QuizApp.Api.Service.AuthService
+{
+    public class AuthService : IAuthService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly JwtTokenHandler _jwtTokenHandler;
+
+        public AuthService(ApplicationDbContext dbContext, JwtTokenHandler jwtTokenHandler)
+        {
+            _dbContext = dbContext;
+            _jwtTokenHandler = jwtTokenHandler;
+        }
+        public async Task<ServiceResponse<LoginResponseDTO>> Login(LoginRequestDTO loginRequest)
+        {
+            var response = new ServiceResponse<LoginResponseDTO>();
+
+            var user = await _dbContext.Users
+                .FirstOrDefaultAsync(u => u.UserName == loginRequest.UserName && u.Password == loginRequest.Password);
+            if (user == null)
+            {
+                response.IsSuccess = false;
+                response.Message = ResponseMessage.InvalidUser;
+            }
+            else
+            {
+                var jwt = _jwtTokenHandler.GenerateJwtToken(loginRequest.UserName);
+                var loginResponse = new LoginResponseDTO
+                {
+                    //JwtToken = jwt,
+                    User=new UserDTO
+                    {
+                        UserId=user.UserId,
+                        UserName=user.UserName
+                    }
+                };
+                response.Data = loginResponse;
+                response.Message = ResponseMessage.LoginSuccess;
+            }
+
+                return response;
+        }
+    }
+}
---
+++ QuizApp.Shared/Models/Dto/LoginResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp.Shared.Models.Dto
+{
+    public class LoginResponseDTO
+    {
+        public JwtDTO JwtToken {  get; set; }
+        public UserDTO User {  get; set; }
+
+    }
+}
