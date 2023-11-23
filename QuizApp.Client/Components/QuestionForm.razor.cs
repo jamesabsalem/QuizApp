2cf06ed9064bde6566cd4834853577ab15c40574
Tania Akther <taniaakther2113@gmail.com>
2023-11-23 11:55:25 +0600

    test

--- QuizApp.Client/Components/QuestionForm.razor.cs
+++ QuizApp.Client/Components/QuestionForm.razor.cs
@@ -4,6 +4,8 @@ namespace QuizApp.Client.Components
 {
     public partial class QuestionForm
     {
+        public Question question = new();
+        public Option option = new();
         private int QuestionType { get; set; } = 1;
     }
 }
